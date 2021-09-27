eeaf1365dd36fa6de6a9c13e2a23445a4949f827
ojedam13 <m_ojeda17@hotmail.com>
2021-09-27 11:05:56 -0300

    citaObj y datosCitas

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+// UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Registar eventos
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCitas);
+    propietarioInput.addEventListener('input', datosCitas);
+    telefonoInput.addEventListener('input', datosCitas);
+    fechaInput.addEventListener('input', datosCitas);
+    horaInput.addEventListener('input', datosCitas);
+    sintomasInput.addEventListener('input', datosCitas);
+}
+
+
+//Objeto con la info de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+// Agrega daos al objeto de cita
+function datosCitas(e) {
+    citaObj[e.target.name] = e.target.value;
+}
